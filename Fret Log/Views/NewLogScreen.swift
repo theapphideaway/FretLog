258af1a69bd8f96920f001903634509c4c13cdcd
Ian Schoenrock <ianschoenrock@gmail.com>
2025-09-25 09:52:14 -0600

    Initial Commit

---
+++ Fret Log/Views/NewLogScreen.swift
@@ -0,0 +1,18 @@
+//
+//  NewLogScreen.swift
+//  Fret Log
+//
+//  Created by ian schoenrock on 9/22/25.
+//
+
+import SwiftUI
+
+struct NewLogScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewLogScreen()
+}
